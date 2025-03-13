4d18737dfebb8d83e47b545857ace36d435c632c
Jahid458 <Jahidul898@outlook.com>
2025-03-13 22:54:40 +0600

    added container

--- src/app/components/home/Faq/Faq.jsx
+++ src/app/components/home/Faq/Faq.jsx
@@ -24,7 +24,7 @@ const Faq = () => {
   const [openIndex, setOpenIndex] = useState(null);
 
   return (
-    <div className="max-w-6xl mx-auto p-4 flex flex-col lg:flex-row lg:items-start lg:gap-10">
+    <div className="container mx-auto p-4 flex flex-col lg:flex-row lg:items-start lg:gap-10">
     <div className="lg:w-1/2  text-center lg:text-left lg:mt-40 mb-6 lg:mb-0 ">
       <span className="bg-primary text-white px-3 py-1 rounded-md text-sm font-semibold dark:text-white">Frequently Asked Questions?</span>
       <h2 className="text-2xl font-bold mt-2">Do you have any questions?</h2>
