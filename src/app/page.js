a4228873e5e35300d9c0a14dfe5daa3ce114608b
unknown <nurmahannadmondol>
2025-03-14 12:35:03 +0430

    update some data

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,21 @@
-import Faq from "./components/home/Faq/Faq";
-import Features from "./components/home/Features";
-import Hero from "./components/home/Hero/Hero";
-import Pricing from "./components/home/Pricing/Pricing";
-import Reviews from "./components/home/Reviews/Reviews";
-import Service from "./components/home/Services/Service";
-
+import Faq from './components/home/Faq/Faq';
+import Features from './components/home/Features';
+import Hero from './components/home/Hero/Hero';
+import LeastStressed from './components/home/Hero/LeastStressed';
+import Pricing from './components/home/Pricing/Pricing';
+import Reviews from './components/home/Reviews/Reviews';
+import Service from './components/home/Services/Service';
 
 export default function Home() {
   return (
-   
     <>
-        <Hero></Hero>
-        <Features></Features>
-        <Service></Service>
-        <Pricing></Pricing>
-        <Reviews></Reviews>
-        <Faq></Faq>
+      <Hero></Hero>
+      <LeastStressed></LeastStressed>
+      <Features></Features>
+      <Service></Service>
+      <Pricing></Pricing>
+      <Reviews></Reviews>
+      <Faq></Faq>
     </>
-
   );
 }
