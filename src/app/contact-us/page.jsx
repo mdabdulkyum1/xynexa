80b682b89681ef5f2659edfb803c1bd758c53eb5
harunbd80 <harun708280@gmail.com>
2025-03-15 01:49:48 +0600

    create contact us

--- src/app/contact-us/page.jsx
+++ src/app/contact-us/page.jsx
@@ -1,8 +1,17 @@
+import BrandSection from "./components/BrandSection";
+import ContactBanner from "./components/ContactBanner";
+import ContactOptions from "./components/ContactOptions";
+import FAQSection from "./components/FAQSection";
+import InsightsSection from "./components/InsightsSection";
 
 const Contact = () => {
     return (
         <div>
-             Contact us
+            <ContactBanner/>
+            <ContactOptions/>
+            <FAQSection/>
+            <BrandSection/>
+            <InsightsSection/>
         </div>
     );
 };
