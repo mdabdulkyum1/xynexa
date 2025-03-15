2769e8c7737a239c001b32642afa2bfbe1815454
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-15 07:30:06 +0600

    meet our team section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import AboutBanner from "./components/AboutBanner";
+import AboutOurTeam from "./components/AboutOurTeam";
 
 const About = () => {
     return (
         <div>
             <AboutBanner></AboutBanner>
+            <AboutOurTeam></AboutOurTeam>
         </div>
     );
 };
