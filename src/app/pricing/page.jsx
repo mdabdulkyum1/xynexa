67b2105675d1f46ff27c3fe75481db92526ab063
Lenovo <jannatjui414@gmail.com>
2025-03-18 19:57:47 +0600

    add faq section for pricing page

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PricingCards from './components/PricingCards';
+import PricingFaq from './components/PricingFaq';
 
 
 
@@ -7,7 +8,7 @@ const Pricing = () => {
   return (
     <div className='mt-20 w-11/12 mx-auto'>
      <PricingCards></PricingCards>
-
+    <PricingFaq></PricingFaq>
     </div>
   );
 };
