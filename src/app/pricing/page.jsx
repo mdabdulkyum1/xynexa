ff6b990ce71a77bfaec1a20cbd7a8d85ded3f8af
Jahid458 <Jahidul898@outlook.com>
2025-03-19 02:59:01 +0600

    packageFeature  section added by Price page

--- src/app/pricing/page.jsx
+++ src/app/pricing/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Productivity from './components/Productivity';
+import PackageFeature from './components/PackageFeature';
 
 const Pricing = () => {
     return (
         <div>
           <Productivity></Productivity>  
+          <PackageFeature></PackageFeature>
         </div>
     );
 };
