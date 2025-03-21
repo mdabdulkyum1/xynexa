deb5c94cb4a703f42920f955a954f4235885f147
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-03-21 10:48:50 +0600

    marged calendar doen

--- src/app/dashboard/meetings/calendar/page.jsx
+++ src/app/dashboard/meetings/calendar/page.jsx
@@ -1,12 +1,10 @@
 
 import MyCalendar from './components/Calendar';
-import CalendarPiker from './components/CalendarPiker';
 
 export default function Page() {
     return (
         <div>
             <h1>Hello, this is the Calendar Page</h1>
-            <CalendarPiker></CalendarPiker>
             <MyCalendar></MyCalendar>
         </div>
     );
