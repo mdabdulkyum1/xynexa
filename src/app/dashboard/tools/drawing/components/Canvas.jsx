c79582ff032d5fc13d8f0124363790a8f9c3fb3c
Jahid458 <Jahidul898@outlook.com>
2025-03-24 12:37:24 +0600

    save functionality added

--- src/app/dashboard/tools/drawing/components/Canvas.jsx
+++ src/app/dashboard/tools/drawing/components/Canvas.jsx
@@ -1,45 +1,62 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Eraser, Icon, Pen, Redo, RotateCcw } from "lucide-react";
+import { Eraser, Icon, Pen, Redo, RotateCcw, Save } from "lucide-react";
 import { useRef, useState } from "react";
 import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 const Canvas = () => {
-    const colorInputRef = useRef(null);
-    const canvasRef = useRef(null);
-    const [strokeColor, setStrokeColor] = useState('#a855f7');
-    const [eraseMode, setEraseMode] = useState(false);
+  const colorInputRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [strokeColor, setStrokeColor] = useState("#a855f7");
+  const [eraseMode, setEraseMode] = useState(false);
 
-        // color fuctionality
-    const handleStrokeColorChange = (e) => {
-        setStrokeColor(e.target.value);
-        console.log(strokeColor);
-    }
-    // Drawing Pen function
-    const handlePenClick = (e) => {
-        setEraseMode(false);
-        canvasRef.current?.eraseMode(false)
-    }
-    // Drawaing earse Function
-    const handleEraseClick = (e) => {
-        setEraseMode(true);
-        canvasRef.current?.eraseMode(true)
-    }
+  // color fuctionality
+  const handleStrokeColorChange = (e) => {
+    setStrokeColor(e.target.value);
+    console.log(strokeColor);
+  };
+  // Drawing Pen function
+  const handlePenClick = (e) => {
+    setEraseMode(false);
+    canvasRef.current?.eraseMode(false);
+  };
+  // Drawaing earse Function
+  const handleEraseClick = (e) => {
+    setEraseMode(true);
+    canvasRef.current?.eraseMode(true);
+  };
 
-    // Undo click
-    const handleUndoClick = (e) => {
-        canvasRef.current?.undo();
-    }
+  // Undo click
+  const handleUndoClick = (e) => {
+    canvasRef.current?.undo();
+  };
 
-    //  Redo Click
+  //  Redo Click
 
-    const handleRedoClick = (e) => {
-        canvasRef.current?.redo();
-    }
+  const handleRedoClick = (e) => {
+    canvasRef.current?.redo();
+  };
+
+
+  // save 
+  const handleSave = async() => {
+    const dataURL = await canvasRef.current?.exportImage('png');
+    if (dataURL) {
+        const link = Object.assign(document.createElement('a'), {
+          href: dataURL,
+          style: { display: 'none' },
+          download: 'xynexadocument.png'
+        });
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+  }
+}
 
   return (
     <div>
-        <hspan className="font-semibold text-[1.5rem] leading-8 sm:text-4xl  sm:leading-tight ">Draw Here</hspan>
-
+      <hspan className="font-semibold text-[1.5rem] leading-8 sm:text-4xl  sm:leading-tight ">
+        Draw Here
+      </hspan>
 
       <div className="mt-6  conatiner mx-auto">
         <ReactSketchCanvas
@@ -52,55 +69,64 @@ const Canvas = () => {
         />
 
         <div>
-       {/* Color picker */}
-       <Button
-          size="icon"
-          onClick={() => colorInputRef.current?.click()}
-          style={{ backgroundColor: strokeColor }}
-        >
-          <input
-            type="color"
-            ref={colorInputRef}
-            className="sr-only"
-            value={strokeColor}
-            onChange={handleStrokeColorChange}
-          />
-        </Button>
+          {/* Color picker */}
+          <Button
+            size="icon"
+            onClick={() => colorInputRef.current?.click()}
+            style={{ backgroundColor: strokeColor }}
+          >
+            <input
+              type="color"
+              ref={colorInputRef}
+              className="sr-only"
+              value={strokeColor}
+              onChange={handleStrokeColorChange}
+            />
+          </Button>
 
-            {/* Drawing Modes  */}
-            <div className="flex flex-col gap-3 pt-6">
-                <Button
-                    size="icon"
-                    variant="outline"
-                    disabled={!eraseMode}
-                    onClick={handlePenClick}
-                ><Pen size={16} /></Button>
-                <Button 
-                size="icon"
-                variant="outline"
-                disabled={eraseMode}
-                onClick={handleEraseClick}
-                ><Eraser size={16} /></Button>
-            </div>
+          {/* Drawing Modes  */}
+          <div className="flex flex-col gap-3 pt-6">
+            <Button
+              size="icon"
+              variant="outline"
+              disabled={!eraseMode}
+              onClick={handlePenClick}
+            >
+              <Pen size={16} />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              disabled={eraseMode}
+              onClick={handleEraseClick}
+            >
+              <Eraser size={16} />
+            </Button>
+          </div>
 
+          {/* Actions  */}
+          <div className="flex flex-col gap-3 pt-6">
+            <Button
+              size="icon"
+              variant="outline"
+              disabled={!eraseMode}
+              onClick={handleUndoClick}
+            >
+              <Redo size={16} />
+            </Button>
+            <Button
+              size="icon"
+              variant="outline"
+              disabled={eraseMode}
+              onClick={handleRedoClick}
+            >
+              <RotateCcw size={16} />
+            </Button>
 
-            {/* Actions  */}
-            <div className="flex flex-col gap-3 pt-6">
-                <Button
-                    size="icon"
-                    variant="outline"
-                    disabled={!eraseMode}
-                    onClick={handleUndoClick}
-                ><Redo size={16} /></Button>
-                <Button 
-                size="icon"
-                variant="outline"
-                disabled={eraseMode}
-                onClick={handleRedoClick}
-                ><RotateCcw size={16} /></Button>
-            </div>
-
-
+            <Button size="icon" variant="outline" onClick={handleSave}>
+              <Save size={16} />
+            </Button>
+          </div>
         </div>
       </div>
     </div>
