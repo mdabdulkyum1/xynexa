2d3e36ca94c6bbf91b5ec85c1a685cfa36e703c9
Jahid458 <Jahidul898@outlook.com>
2025-03-24 12:06:31 +0600

    color picker added in stroke color button

--- src/app/dashboard/tools/drawing/components/Canvas.jsx
+++ src/app/dashboard/tools/drawing/components/Canvas.jsx
@@ -1,16 +1,49 @@
 "use client";
-import { ReactSketchCanvas } from "react-sketch-canvas";
+import { Button } from "@/components/ui/button";
+import { Icon } from "lucide-react";
+import { useRef, useState } from "react";
+import { ReactSketchCanvas, ReactSketchCanvasRef } from "react-sketch-canvas";
 const Canvas = () => {
+    const colorInputRef = useRef(null);
+    const canvasRef = useRef(null);
+    const [strokeColor, setStrokeColor] = useState('#a855f7');
+    const [eraseMode, setEraseMode] = useState(false);
+
+
+    const handleStrokeColorChange = (e) => {
+        setStrokeColor(e.target.value)
+    }
   return (
     <div>
-        <h1 className="text-center lg:text-4xl md:text-2xl text-sm">Draw Here</h1>
-      <div>
+        <hspan className="font-semibold text-[1.5rem] leading-8 sm:text-4xl  sm:leading-tight ">Draw Here</hspan>
+
+
+      <div className="mt-6  conatiner mx-auto">
         <ReactSketchCanvas
-          width="88%"
+          width="100%"
           height="500px"
+          ref={canvasRef}
           canvasColor="transparent"
           strokeColor="#000"
+          className="!rounded-2xl !border-purple-500 dark:!border-purple-800"
         />
+
+        <div>
+       {/* Color picker */}
+       <Button
+          size="icon"
+          onClick={() => colorInputRef.current?.click()}
+          style={{ backgroundColor: strokeColor }}
+        >
+          <input
+            type="color"
+            ref={colorInputRef}
+            className="sr-only"
+            value={strokeColor}
+            onChange={handleStrokeColorChange}
+          />
+        </Button>
+        </div>
       </div>
     </div>
   );
