ba8bc9cea76683e3a93826ae6c148bfa817ab0d5
harun708280 <harun708280@gmail.com>
2025-03-29 14:43:11 +0600

    uo

--- src/app/dashboard/chat/page.jsx
+++ src/app/dashboard/chat/page.jsx
@@ -4,6 +4,10 @@ export default function Page() {
     return (
         <div>
             <h1>Hello, this is the Chat Page</h1>
+            <h1>Hello, this is the Chat Page</h1>
+            <h1>Hello, this is the Chat Page</h1>
+            <h1>Hello, this is the Chat Page</h1>
+            <h1>Hello, this is the Chat Page</h1>
         </div>
     );
 }
