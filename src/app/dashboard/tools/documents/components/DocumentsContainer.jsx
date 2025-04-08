3737239f255ded372668098ffcb444de916761b6
Lenovo <jannatjui414@gmail.com>
2025-04-08 22:44:43 +0600

    change color for dark mood

--- src/app/dashboard/tools/documents/components/DocumentsContainer.jsx
+++ src/app/dashboard/tools/documents/components/DocumentsContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DocumentsContainer = () => {
     return (
-        <div className='p-4 lg:p-8 border-2 border-red-600'>
+        <div className='p-4 lg:p-8 border-2'>
             <h2 className='text-xl font-bold lg:text-3xl text-gray-700 dark:text-white'>Your Documents</h2>
         </div>
     );
