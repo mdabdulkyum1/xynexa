48d562ea9c1f9a54d02c3bbe8815d93f061069cc
harun708280 <harun708280@gmail.com>
2025-04-17 19:25:49 +0600

    overvie layout

--- src/app/dashboard/component/Dashboard/WelcomeBanner.jsx
+++ src/app/dashboard/component/Dashboard/WelcomeBanner.jsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const WelcomeBanner = () => {
   return (
     <div className="bg-white dark:bg-black rounded-lg p-8 shadow-md flex items-center justify-between">
       <div className="flex-1">
-        <p className="text-sm text-gray-500">Welcome To</p>
-        <h2 className="text-3xl font-semibold mb-4">Your Task Management Area</h2>
+        <p className="text-sm text-gray-500">Welcome to</p>
+        <h2 className="text-3xl font-semibold mb-4">
+          XYnexa Collaboration Hub
+        </h2>
         <p className="text-gray-600 mb-6">
-          Lorem ipsum dolor sit amet consectetur. Bibendum risus urna tortor praesent.
+          Streamline your team's workflow and boost productivity with powerful
+          task management tools built for seamless collaboration.
         </p>
-        <button className="bg-purple-600 hover:bg-purple-700 text-white font-semibold py-2 px-6 rounded-full">
+        <button className="bg-primary  text-white font-semibold py-2 px-6 rounded-lg">
           Learn More
         </button>
       </div>
-      <div className="flex-1 h-[200px] my-6 flex justify-end">
+      <div className="flex-1  h-[200px] my-[12px] flex justify-end">
         <img
-          src="/assets/feature.jpg" 
+          src="/assets/feature.jpg"
           alt="Welcome"
           className="max-w-md rounded-2xl"
         />
