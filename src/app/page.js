2b8df4363635e4910467984bb7d92b251d376983
Lenovo <jannatjui414@gmail.com>
2025-04-20 20:56:49 +0600

    add 6 section in landing page

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
      <EssyCollab></EssyCollab>
      <WorkFlow></WorkFlow>
      <FavriteApps></FavriteApps>
-     <Today></Today>
+    
 
       
       {/* <LeastStressed></LeastStressed> */}
