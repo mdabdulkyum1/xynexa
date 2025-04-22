7bf1faba2f324c30b521d340fda090b4ae36ec18
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-04-23 00:04:30 +0600

    fix final

--- src/app/dashboard/team/view/page.jsx
+++ src/app/dashboard/team/view/page.jsx
@@ -57,16 +57,16 @@ export default function TeamView() {
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 lg:p-8  bg-red-100 dark:bg-black'>
                 
                 <CreateModalBtn></CreateModalBtn>
-                {teamData && teamData.length > 0 ? (
-                    teamData.map((team, idx) => (
+                {teamData && teamData?.length > 0 ? (
+                    teamData?.map((team, idx) => (
                        
                          <div key={idx} className="p-4 rounded-lg shadow bg-white dark:bg-gray-700">
                             <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-2">
-                                {team.name}
+                                {team?.name}
                             </h2>
                             <div className="flex items-center mb-3">
                                 <Image
-                                    src={team.creator.imageUrl}
+                                    src={team?.creator?.imageUrl}
                                     alt="Creator Image"
                                     width={40}
                                     height={40}
@@ -74,10 +74,10 @@ export default function TeamView() {
                                 />
                                 <div>
                                     <p className="text-sm font-semibold text-gray-800 dark:text-gray-200">
-                                        {team.creator.firstName} {team.creator.lastName}
+                                        {team?.creator?.firstName} {team?.creator?.lastName}
                                     </p>
                                     <p className="text-xs text-gray-500 dark:text-gray-300">
-                                        {team.creator.email}
+                                        {team?.creator?.email}
                                     </p>
                                 </div>
                             </div>
@@ -85,14 +85,14 @@ export default function TeamView() {
                             
                             <div className="flex items-center text-gray-600 dark:text-gray-300 mb-2">
                                 <Users className="mr-2 w-4 h-4" /> 
-                                {team.members.length} Members
+                                {team?.members?.length} Members
                             </div>
                             <div className="flex justify-between items-center text-gray-600 dark:text-gray-300">
                                 <p className='flex items-center'>
                                 <Calendar className="mr-2 w-4 h-4" /> 
-                                Created: {new Date(team.createdAt).toLocaleDateString()}
+                                Created: {new Date(team?.createdAt).toLocaleDateString()}
                                 </p>
-                                <button className='btn bg-primary text-white dark:text-black dark:bg-white border-none'><Link  key={team._id} href={`/dashboard/team/view/${team._id}`}> Details </Link></button>
+                                <button className='btn bg-primary text-white dark:text-black dark:bg-white border-none'><Link  key={team._id} href={`/dashboard/team/view/${team?._id}`}> Details </Link></button>
                             </div>
 
                         </div>
