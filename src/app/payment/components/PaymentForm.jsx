55177c8238580a01af8125c67931b4acd2586f41
Lenovo <jannatjui414@gmail.com>
2025-04-24 00:19:05 +0600

    add suspense

--- src/app/payment/components/PaymentForm.jsx
+++ src/app/payment/components/PaymentForm.jsx
@@ -6,6 +6,8 @@ import { loadStripe } from '@stripe/stripe-js';
 import { useSearchParams } from 'next/navigation';
 import CheckoutForm from './CheckoutForm';
 import { Elements } from '@stripe/react-stripe-js';
+import { Suspense } from 'react';
+import Loading from '@/components/loading/Loading';
 
 
 
@@ -26,7 +28,9 @@ export default function PaymentForm() {
     return (
         <div className="max-w-md mx-auto p-4">
             <Elements stripe={stripePromise}>
+            <Suspense fallback={<div><Loading></Loading></div>}>
             <CheckoutForm amount={amount} plan={plan} />
+            </Suspense>
             </Elements>
         </div>
     );
