bf986002f84ebac86d48a61613fd179d8b70d5cc
harun708280 <harun708280@gmail.com>
2025-04-28 16:15:15 +0600

    update admin-dashbaord

--- src/app/admin-dashbaord/page.js
+++ src/app/admin-dashbaord/page.js
@@ -1,23 +1,18 @@
-
-
-
-
-import { SectionCards } from "./component/section-cards"
-import { ChartAreaInteractive } from "./component/chart-area-interactive"
+import { SectionCards } from "./component/section-cards";
+import { ChartAreaInteractive } from "./component/chart-area-interactive";
 
 // hay
 export default function Page() {
   return (
     <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards/>
-              <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
-              </div>
-              
-            </div>
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <SectionCards />
+          <div className="px-4 lg:px-6">
+            <ChartAreaInteractive />
           </div>
         </div>
-  )
+      </div>
+    </div>
+  );
 }
