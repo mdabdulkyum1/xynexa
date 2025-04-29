b4e78840feef6a0639e38e168556fd0641c50a33
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-04-29 23:11:18 +0600

    fix error

--- src/app/payment/stripe/page.jsx
+++ src/app/payment/stripe/page.jsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { Suspense } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import { useSearchParams } from 'next/navigation';
 import CheckoutForm from '../components/CheckoutForm';
+import Loading from '@/components/loading/Loading';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
@@ -21,6 +23,7 @@ export default function StripePage() {
   }
 
   return (
+    <Suspense fallback={<div><Loading></Loading></div>}>
     <div className="min-h-screen flex items-center justify-center bg-[#f0f4f8] p-4">
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
         <h2 className="text-2xl font-bold mb-4 text-[#014E4E] text-center">
@@ -31,5 +34,6 @@ export default function StripePage() {
         </Elements>
       </div>
     </div>
+    </Suspense>
   );
 }
