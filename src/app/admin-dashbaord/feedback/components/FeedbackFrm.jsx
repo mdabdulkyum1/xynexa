f562813342b433693b3aa2f63777a2c1806474e3
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-04-30 16:58:07 +0600

    b

--- src/app/admin-dashbaord/feedback/components/FeedbackFrm.jsx
+++ src/app/admin-dashbaord/feedback/components/FeedbackFrm.jsx
@@ -1,7 +1,11 @@
-"use client";
+'use client';
 import React from 'react';
-import feedback from '../../../../../public/assets/lottie/feedback.json'
-import Lottie from 'lottie-react';
+import feedback from '../../../../../public/assets/lottie/feedback.json';
+import dynamic from 'next/dynamic';
+
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
+
 const FeedbackFrm = () => {
     return (
         <div>
@@ -11,11 +15,11 @@ const FeedbackFrm = () => {
                         <h1 className="text-3xl lg:text-4xl font-bold">Share Your Thoughts With Us</h1>
                         
                         <div>
-                            <Lottie animationData={feedback}></Lottie>
+                            <Lottie animationData={feedback} />
                         </div>
                     </div>
                     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                        <div className="card-body ">
+                        <div className="card-body">
                             <p className='font-bold text-xl'>We’d Love to Hear From You</p>
                             <fieldset className="fieldset space-y-1">
                                 <label className="label">Name</label>
@@ -23,8 +27,7 @@ const FeedbackFrm = () => {
                                 <label className="label">Email</label>
                                 <input type="email" className="input bg-gray-200" placeholder="Email" />
                                 <label className="label">Your Feedback</label>
-                                <textarea className="textarea  bg-gray-200" placeholder="write something"></textarea>
-                               
+                                <textarea className="textarea bg-gray-200" placeholder="write something"></textarea>
                                 <button className="btn bg-primary text-white mt-4 border-none rounded-lg">Submit</button>
                             </fieldset>
                         </div>
