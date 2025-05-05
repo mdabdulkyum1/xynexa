46808b4c57ec7c9412f0f74a28e421dd6f421a04
mdabdulkyum1 <kyummdabdul@gmail.com>
2025-05-05 20:06:24 +0600

    fix dark light

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
           },
         }}
       >
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
           <body
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
