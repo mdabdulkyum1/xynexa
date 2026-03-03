953cc812f6f46c20107899446e633c7f2be32c62
mdabdulkyum1 <kyummdabdul@gmail.com>
2026-03-04 00:57:46 +0600

    update auth-secret

--- src/hooks/AxiosPublic/useAxiosPublic.jsx
+++ src/hooks/AxiosPublic/useAxiosPublic.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://xynexa-nest.onrender.com/api/v1'
-  // baseURL: 'http://localhost:5000/api/v1'
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000/api/v1'
 });
 
 
